ec0cb654e6dad272b3a9aea3b9d8d8c129b25a70
Gordon Watts <gwatts@uw.edu>
2024-05-01 09:18:25 -0700

    Move current code into a separate query library file

---
+++ servicex/query_library.py
@@ -0,0 +1,151 @@
+import logging
+from func_adl_servicex_xaodr22 import (
+    FuncADLQueryPHYSLITE,
+    cpp_float,
+    cpp_int,
+    cpp_vfloat,
+    cpp_vint,
+    atlas_release,
+)
+
+
+def query_all() -> FuncADLQueryPHYSLITE:
+    # Because we are going to do a specialized query, we'll alter the return type here.
+    logging.info(f"Using release {atlas_release} for type information.")
+
+    ds = FuncADLQueryPHYSLITE()
+
+    # Build the query
+    # TODO: The EventInfo argument should default correctly
+    #       (that may just be a matter of using func_adl xaod r22)
+    # TODO: dataclass should be supported so as not to lose type-following!
+    # TODO: once https://github.com/iris-hep/func_adl/issues/136 fixed, turn off fmt off.
+    # fmt: off
+    query = (ds.Select(lambda e: {
+            "evt": e.EventInfo("EventInfo"),
+            "jet": e.Jets(),
+        })
+        .Select(lambda ei: {
+            "event_number": ei.evt.eventNumber(),  # type: ignore
+            "run_number": ei.evt.runNumber(),  # type: ignore
+            "jet_pt": ei.jet.Select(lambda j: j.pt() / 1000),  # type: ignore
+            "jet_eta": ei.jet.Select(lambda j: j.eta()),  # type: ignore
+            "jet_phi": ei.jet.Select(lambda j: j.phi()),  # type: ignore
+            "jet_m": ei.jet.Select(lambda j: j.m()),  # type: ignore
+            "jet_EnergyPerSampling":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_vfloat]("EnergyPerSampling")
+                ),
+            "jet_SumPtTrkPt500":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_vfloat]("SumPtTrkPt500")
+                ),
+            "jet_TrackWidthPt1000":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_vfloat]("TrackWidthPt1000")
+                ),
+            "jet_NumTrkPt500":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_vint]("NumTrkPt500")
+                ),
+            "jet_NumTrkPt1000":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_vint]("NumTrkPt1000")
+                ),
+            "jet_SumPtChargedPFOPt500":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_vfloat]("SumPtChargedPFOPt500")
+                ),
+            "jet_Timing":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("Timing")
+                ),
+            "jet_JetConstitScaleMomentum_eta":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("JetConstitScaleMomentum_eta")
+                ),
+            "jet_ActiveArea4vec_eta":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("ActiveArea4vec_eta")
+                ),
+            "jet_DetectorEta":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("DetectorEta")
+                ),
+            "jet_JetConstitScaleMomentum_phi":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("JetConstitScaleMomentum_phi")
+                ),
+            "jet_ActiveArea4vec_phi":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("ActiveArea4vec_phi")
+                ),
+            "jet_JetConstitScaleMomentum_m":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("JetConstitScaleMomentum_m")
+                ),
+            "jet_JetConstitScaleMomentum_pt":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("JetConstitScaleMomentum_pt")
+                ),
+            "jet_Width":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("Width")
+                ),
+            "jet_EMFrac":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("EMFrac")
+                ),
+            "jet_ActiveArea4vec_m":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("ActiveArea4vec_m")
+                ),
+            "jet_DFCommonJets_QGTagger_TracksWidth":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("DFCommonJets_QGTagger_TracksWidth")
+                ),
+            "jet_JVFCorr":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("JVFCorr")
+                ),
+            "jet_DFCommonJets_QGTagger_TracksC1":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("DFCommonJets_QGTagger_TracksC1")
+                ),
+            "jet_PSFrac":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("PSFrac")
+                ),
+            "jet_DFCommonJets_QGTagger_NTracks":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_int]("DFCommonJets_QGTagger_NTracks")
+                ),
+            "jet_DFCommonJets_fJvt":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("DFCommonJets_fJvt")
+                ),
+            "jet_PartonTruthLabelID":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_int]("PartonTruthLabelID")
+                ),
+            "jet_HadronConeExclExtendedTruthLabelID":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_int]("HadronConeExclExtendedTruthLabelID")
+                ),
+            "jet_ConeTruthLabelID":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_int]("ConeTruthLabelID")
+                ),
+            "jet_HadronConeExclTruthLabelID":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_int]("HadronConeExclTruthLabelID")
+                ),
+            "jet_ActiveArea4vec_pt":
+                ei.jet.Select(  # type: ignore
+                    lambda j: j.getAttribute[cpp_float]("ActiveArea4vec_pt")
+                ),
+        })
+    )
+    # fmt: on
+
+    return query
--- servicex/servicex_materialize_branches.py
+++ servicex/servicex_materialize_branches.py
@@ -10,16 +10,10 @@ import dask
 import dask_awkward as dak
 import uproot
 from dask.distributed import Client, LocalCluster, performance_report
-from func_adl_servicex_xaodr22 import (
-    FuncADLQueryPHYSLITE,
-    atlas_release,
-    cpp_float,
-    cpp_int,
-    cpp_vfloat,
-    cpp_vint,
-)
 
 import servicex as sx
+
+from query_library import query_all
 
 
 class ElapsedFormatter(logging.Formatter):
@@ -50,141 +44,8 @@ def query_servicex(
     else:
         logging.info(f"Running on {num_files} files of dataset.")
 
-    # Because we are going to do a specialized query, we'll alter the return type here.
-    ds = FuncADLQueryPHYSLITE()
-
     # Build the query
-    # TODO: The EventInfo argument should default correctly
-    #       (that may just be a matter of using func_adl xaod r22)
-    # TODO: dataclass should be supported so as not to lose type-following!
-    # TODO: once https://github.com/iris-hep/func_adl/issues/136 fixed, turn off fmt off.
-    # fmt: off
-    query = (ds.Select(lambda e: {
-            "evt": e.EventInfo("EventInfo"),
-            "jet": e.Jets(),
-        })
-        .Select(lambda ei: {
-            "event_number": ei.evt.eventNumber(),  # type: ignore
-            "run_number": ei.evt.runNumber(),  # type: ignore
-            "jet_pt": ei.jet.Select(lambda j: j.pt() / 1000),  # type: ignore
-            "jet_eta": ei.jet.Select(lambda j: j.eta()),  # type: ignore
-            "jet_phi": ei.jet.Select(lambda j: j.phi()),  # type: ignore
-            "jet_m": ei.jet.Select(lambda j: j.m()),  # type: ignore
-            "jet_EnergyPerSampling":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_vfloat]("EnergyPerSampling")
-                ),
-            "jet_SumPtTrkPt500":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_vfloat]("SumPtTrkPt500")
-                ),
-            "jet_TrackWidthPt1000":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_vfloat]("TrackWidthPt1000")
-                ),
-            "jet_NumTrkPt500":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_vint]("NumTrkPt500")
-                ),
-            "jet_NumTrkPt1000":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_vint]("NumTrkPt1000")
-                ),
-            "jet_SumPtChargedPFOPt500":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_vfloat]("SumPtChargedPFOPt500")
-                ),
-            "jet_Timing":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("Timing")
-                ),
-            "jet_JetConstitScaleMomentum_eta":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("JetConstitScaleMomentum_eta")
-                ),
-            "jet_ActiveArea4vec_eta":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("ActiveArea4vec_eta")
-                ),
-            "jet_DetectorEta":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("DetectorEta")
-                ),
-            "jet_JetConstitScaleMomentum_phi":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("JetConstitScaleMomentum_phi")
-                ),
-            "jet_ActiveArea4vec_phi":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("ActiveArea4vec_phi")
-                ),
-            "jet_JetConstitScaleMomentum_m":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("JetConstitScaleMomentum_m")
-                ),
-            "jet_JetConstitScaleMomentum_pt":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("JetConstitScaleMomentum_pt")
-                ),
-            "jet_Width":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("Width")
-                ),
-            "jet_EMFrac":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("EMFrac")
-                ),
-            "jet_ActiveArea4vec_m":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("ActiveArea4vec_m")
-                ),
-            "jet_DFCommonJets_QGTagger_TracksWidth":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("DFCommonJets_QGTagger_TracksWidth")
-                ),
-            "jet_JVFCorr":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("JVFCorr")
-                ),
-            "jet_DFCommonJets_QGTagger_TracksC1":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("DFCommonJets_QGTagger_TracksC1")
-                ),
-            "jet_PSFrac":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("PSFrac")
-                ),
-            "jet_DFCommonJets_QGTagger_NTracks":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_int]("DFCommonJets_QGTagger_NTracks")
-                ),
-            "jet_DFCommonJets_fJvt":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("DFCommonJets_fJvt")
-                ),
-            "jet_PartonTruthLabelID":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_int]("PartonTruthLabelID")
-                ),
-            "jet_HadronConeExclExtendedTruthLabelID":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_int]("HadronConeExclExtendedTruthLabelID")
-                ),
-            "jet_ConeTruthLabelID":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_int]("ConeTruthLabelID")
-                ),
-            "jet_HadronConeExclTruthLabelID":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_int]("HadronConeExclTruthLabelID")
-                ),
-            "jet_ActiveArea4vec_pt":
-                ei.jet.Select(  # type: ignore
-                    lambda j: j.getAttribute[cpp_float]("ActiveArea4vec_pt")
-                ),
-        })
-    )
-    # fmt: on
+    query = query_all()
 
     # Do the query.
     # TODO: Where is the enum that does DeliveryEnum come from?
@@ -230,8 +91,6 @@ def main(
     Load all the branches from some dataset, and then count the flattened
     number of items, and, finally, print them out.
     """
-    logging.info(f"Using release {atlas_release} for type information.")
-
     # Make sure there is a file here to save the SX query ID's to
     # improve performance!
     sx_query_ids = Path("./servicex_query_cache.json")
