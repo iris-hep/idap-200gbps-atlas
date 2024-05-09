4ce72280a35542344cf04a7600e4f4d28cce0b70
Alexander Held <alexander.held@cern.ch>
2024-05-09 21:28:11 +0200

    update utility module

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,14 @@
 import datetime
+import json
+import os
+import pathlib
 import threading
 import time
-import pathlib
-import os
+
+import awkward as ak
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import numpy as np
 
 
 FNAME_OUT = "num_workers.txt"
@@ -20,21 +24,21 @@ def write_num_workers(fname, client, interval):
             time.sleep(interval)
 
 
-def start_tracking_workers(client, interval=1):
-    pathlib.Path('DASK_RUNNING').touch()  # file that indicates worker tracking should run
-    nworker_thread = threading.Thread(target=write_num_workers, args=("num_workers.txt", client, interval))
+def start_tracking_workers(client, measurement_path, interval=1):
+    pathlib.Path(FNAME_FLAG).touch()  # file that indicates worker tracking should run
+    nworker_thread = threading.Thread(target=write_num_workers, args=(measurement_path / FNAME_OUT, client, interval))
     nworker_thread.start()  # track workers in background
     return nworker_thread
 
 
 def stop_tracking_workers():
-    pathlib.Path('DASK_RUNNING').unlink()
-
-
-def get_timestamps_and_counts():
-    print("sleeping for two seconds before reading out file to ensure tracking is done")
+    pathlib.Path(FNAME_FLAG).unlink()
+    print("sleeping for two seconds before proceeding to ensure tracking is done")
     time.sleep(2)
-    with open("num_workers.txt") as f:
+
+
+def get_timestamps_and_counts(measurement_path):
+    with open(measurement_path / FNAME_OUT) as f:
         content = [l.strip().split(", ") for l in f.readlines()]
 
     timestamps = [datetime.datetime.strptime(c[0],  '%Y-%m-%d %H:%M:%S.%f') for c in content]
@@ -47,15 +51,53 @@ def get_timestamps_and_counts():
     return timestamps, nworkers, avg_num_workers
 
 
-def plot_worker_count(timestamps, nworkers, avg_num_workers):
-    fig, ax = plt.subplots()
-    ax.plot(timestamps, nworkers)
-    ax.set_xlabel("time")
-    ax.set_ylabel("number of workers")
-    ax.set_ylim([0, ax.get_ylim()[1]*1.2])
-    ax.set_title(f"worker count over time, average: {avg_num_workers:.1f}")
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+def save_measurement(out, t0, t1, measurement_path):
+    # save results to json
+    with open(measurement_path / "out.json", "w") as f:
+        f.write(ak.to_json(out, convert_other=str))
 
-    for tick in ax.get_xticklabels():
+    with open(measurement_path / "start_end_time.txt", "w") as f:
+        f.write(f"{t0},{t1}")
+
+
+def load_measurement(measurement_path):
+    # load results from json (datetime becomes str)
+    with open(measurement_path / "out.json") as f:
+        content = f.readlines()
+
+    out = ak.from_json(content[0])
+    out["time_finished"] = ak.Array(np.asarray(out["time_finished"], dtype=np.datetime64))  # convert back to np.datetime64
+
+    with open(measurement_path / "start_end_time.txt") as f:
+        t0, t1 = [float(t) for t in f.readlines()[0].split(",")]
+
+    return out, t0, t1
+
+
+def save_fileset(fileset, measurement_path):
+    fileset = dict((k, dict(v.items())) for k, v in fileset.items())  # defaultdict to dict
+    with open(measurement_path / "fileset.json", "w") as f:
+        json.dump(fileset, f, sort_keys=True, indent=4)
+
+
+def plot_worker_count(timestamps, nworkers, avg_num_workers, times_for_rates, instantaneous_rates, measurement_path):
+    fig, ax1 = plt.subplots(constrained_layout=True)
+    ax1.plot(timestamps, nworkers, linewidth=2, color="C0")
+    ax1.set_xlabel("time")
+    ax1.set_ylabel("number of workers", color="C0")
+    ax1.set_ylim([0, ax1.get_ylim()[1]*1.1])
+    # ax1.set_title(f"worker count over time, average: {avg_num_workers:.1f}")
+    ax1.set_title(f"worker count and data rate over time")
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+    ax1.tick_params(axis="y", labelcolor="C0")
+
+    for tick in ax1.get_xticklabels():
         tick.set_rotation(45)
         tick.set_horizontalalignment("right")
+
+    ax2 = ax1.twinx()
+    ax2.plot(times_for_rates, instantaneous_rates, marker="v", linewidth=0, color="C1")
+    ax2.set_ylabel("data rate [Gbps]", color="C1")
+    ax2.set_ylim([0, ax2.get_ylim()[1]*1.1])
+    ax2.tick_params(axis="y", labelcolor="C1")
+    fig.savefig(measurement_path / "worker_count_data_rate_over_time.pdf")
